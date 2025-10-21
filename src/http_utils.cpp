bb28c95d6b1fdc0fc06c97a8b2f1eaa3c0b86f84
Baldeep <baldeep.gill@kcl.ac.uk>
2025-10-21 19:32:10 +0100

    Add curl flags to allow reuse of connections

--- src/http_utils.cpp
+++ src/http_utils.cpp
@@ -27,6 +27,8 @@ std::string http_get(const std::string& url) {
     curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+    curl_easy_setopt(curl, CURLOPT_FRESH_CONNECT, 0L);
+    curl_easy_setopt(curl, CURLOPT_FORBID_REUSE, 0L);
 
     CURLcode res = curl_easy_perform(curl);
     if (res != CURLE_OK) std::cerr << "CURL error (" << url << "): " << curl_easy_strerror(res) << "\n";
