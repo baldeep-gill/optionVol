247c12dc1441dc58670fecf20ab9b63b2fbb6ea7
Baldeep <baldeep.gill@kcl.ac.uk>
2025-10-22 18:48:47 +0100

    Implement ThreadPool class

---
+++ include/thread_pool.h
@@ -0,0 +1,80 @@
+#ifndef THREAD_POOL_H
+#define THREAD_POOL_H
+
+#include <vector>
+#include <thread>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <future>
+#include <functional>
+#include <atomic>
+
+class ThreadPool {
+    public:
+        explicit ThreadPool(size_t thread_count);
+        ~ThreadPool();
+
+        template<class F, class... Args>
+        auto enqueue(F&& f, Args&&... args) -> std::future<decltype(f(args...))>;
+
+    private:
+        std::vector<std::thread> workers;
+        std::queue<std::function<void()>> tasks;
+        std::mutex queue_mutex;
+        std::condition_variable cv;
+        bool stop;
+};
+
+inline ThreadPool::ThreadPool(size_t thread_count) : stop(false) {
+    for (size_t i = 0; i < thread_count; ++i) {
+        workers.emplace_back([this] {
+            while (true) {
+                std::function<void()> task;
+                {
+                    std::unique_lock<std::mutex> lock(this->queue_mutex);
+
+                    this->cv.wait(lock, 
+                        [this]{ return this->stop || !this->tasks.empty(); });
+
+                    if (this->stop && this->tasks.empty()) return;
+
+                    task = std::move(this->tasks.front());
+                    this->tasks.pop();
+                }
+                task();
+            }
+        });
+    }
+}
+
+inline ThreadPool::~ThreadPool() {
+    {
+        std::unique_lock<std::mutex> lock(queue_mutex);
+        stop = true;
+    }
+    cv.notify_all();
+    for (std::thread& worker: workers) {
+        worker.join();
+    }
+}
+
+template<class F, class... Args>
+auto ThreadPool::enqueue(F&& f, Args&&... args) -> std::future<decltype(f(args...))> {
+    using return_type = decltype(f(args...));
+    auto task = std::make_shared<std::packaged_task<return_type()>>(
+        std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+    );
+
+    std::future<return_type> res = task->get_future();
+    {
+        std::unique_lock<std::mutex> lock(queue_mutex);
+        if (stop) throw std::runtime_error("enqueue on stopped ThreadPool");
+        tasks.emplace([task](){ (*task)(); });
+    }
+
+    cv.notify_one();
+    return res;
+}
+
+#endif
